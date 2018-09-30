a0575f2f08389985ec6901d7aea270992862e30c
Rajan <rajsaminathan@gmail.com>
2018-09-30 06:54:43 +0530

    NYTimesMostPopularArticles
    NYTimesMostPopularArticles codes are commited.

---
+++ NYTimesMostPopularArticles/app/src/main/java/com/newyorktimes/utils/Constant.java
@@ -0,0 +1,12 @@
+package com.newyorktimes.utils;
+
+/**
+ * Created by Rajan on 29-09-2018.
+ */
+
+public class Constant {
+
+    public static final String BUNDLE_RESPONSE = "bundleResponse";
+    public static final String BUNDLE_ARTICLE_URL = "articleURL";
+
+}
